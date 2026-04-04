6122cc993cc9ed454ea6a5a3ec52a70f4c1f2086
mridulgridsphere-cmyk <mridul.gridsphere@gmail.com>
2026-04-04 16:20:54 +0530

    fixed the setup issues and small bug in the session.py

--- pos-backend/app/db/session.py
+++ pos-backend/app/db/session.py
@@ -2,11 +2,16 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
+# Construct the URL
 DATABASE_URL = f"mysql+aiomysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
 engine = create_async_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    # ADD THIS SECTION BELOW:
+    connect_args={
+        "auth_plugin": "caching_sha2_password"
+    }
 )
 
 async_session = sessionmaker(
